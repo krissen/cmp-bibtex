d1fb6e06c8d3e33b261a8b98abdd9cd58ca8a179
Kristian Niemi <kristian.niemi@gmail.com>
2025-06-02 22:13:31 +0200

    bugfix

--- after/plugin/cmp-bibtex.lua
+++ after/plugin/cmp-bibtex.lua
@@ -1,1 +1,5 @@
-require('cmp').register_source("bibtex", require("cmp-bibtex").new())
+local ok_cmp, cmp = pcall(require, "cmp")
+local ok_bib, cmp_bibtex = pcall(require, "cmp-bibtex")
+if ok_cmp and ok_bib and cmp_bibtex and cmp_bibtex.new then
+  cmp.register_source("bibtex", cmp_bibtex.new())
+end
